f64454c677311ee21b116b98775448c8827a6e61
Deon Okonkwo <deondazy@gmail.com>
2025-07-26 23:16:48 +0100

    refactor: Remove custom constructor from InvalidHandlerException for simplicity

--- src/Exceptions/InvalidHandlerException.php
+++ src/Exceptions/InvalidHandlerException.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace Denosys\Routing\Exceptions;
 
-use Throwable;
 use InvalidArgumentException;
 
 class InvalidHandlerException extends InvalidArgumentException
 {
-    public function __construct($handler, int $code = 0, ?Throwable $previous = null)
-    {
-        $type = is_object($handler) ? get_class($handler) : gettype($handler);
-        parent::__construct("Invalid route handler of type {$type}.", $code, $previous);
-    }
 }
