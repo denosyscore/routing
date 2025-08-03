da3597b2a1d8c12f1f3a00c4f35d453d1a67ad13
Deon Okonkwo <deondazy@gmail.com>
2025-08-03 16:00:55 +0100

    feat: remove redundant absolute parameter from UrlGeneratorInterface::to method

--- src/UrlGeneratorInterface.php
+++ src/UrlGeneratorInterface.php
@@ -22,7 +22,7 @@ interface UrlGeneratorInterface
     public function getRequest(): ?ServerRequestInterface;
     public function current(bool $includeQuery = true): string;
     public function previous(?string $fallback = null): string;
-    public function to(string $path, array $query = [], bool $secure = false, bool $absolute = true): string;
+    public function to(string $path, array $query = [], bool $secure = false): string;
     public function asset(string $path, ?string $version = null, bool $secure = false, bool $absolute = true): string;
     public function full(): string;
     public function signedRoute(BackedEnum|string $name, array $parameters = [], DateTimeInterface|DateInterval|int|null $expiration = null, bool $absolute = true): string;
