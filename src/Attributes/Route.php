291e5e65bebebc0cecbd85acae8bea604d9b3220
Deon Okonkwo <deondazy@gmail.com>
2025-08-23 14:07:52 +0100

    feat: add abstract Route class for defining routing attributes

---
+++ src/Attributes/Route.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Denosys\Routing\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_CLASS)]
+abstract class Route
+{
+    public function __construct(
+        public readonly string $path = '',
+        public readonly ?string $name = null,
+        public readonly array $where = [],
+        public readonly array $middleware = [],
+    ) {}
+
+    abstract public function getMethods(): array;
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getWhere(): array
+    {
+        return $this->where;
+    }
+
+    public function getMiddleware(): array
+    {
+        return $this->middleware;
+    }
+}
