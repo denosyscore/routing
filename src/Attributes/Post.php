56464b4ff133f7d95b42ac766bffc34043f44c30
Deon Okonkwo <deondazy@gmail.com>
2025-08-23 14:07:11 +0100

    feat: add Post attribute for defining POST HTTP method in routing

---
+++ src/Attributes/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Denosys\Routing\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_METHOD)]
+class Post extends Route
+{
+    public function getMethods(): array
+    {
+        return ['POST'];
+    }
+}
