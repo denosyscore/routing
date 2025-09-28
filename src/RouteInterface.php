e647e267c797a9f212516dfea84179487e1b7e85
Deon Okonkwo <deondazy@gmail.com>
2025-09-28 04:18:32 +0100

    refactor: simplify middleware methods in RouteInterface and remove unused imports

--- src/RouteInterface.php
+++ src/RouteInterface.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Denosys\Routing;
 
-use Closure;
-use Psr\Http\Server\MiddlewareInterface;
-
 interface RouteInterface
 {
     public function getMethods(): array;
@@ -18,12 +15,10 @@ interface RouteInterface
     public function getParameters(string $path): array;
     
     // Middleware methods
-    public function middleware(MiddlewareInterface|array|string $middleware, int $priority = 0): static;
-    public function middlewareWhen(bool|Closure $condition, MiddlewareInterface|array|string $middleware, int $priority = 0): static;
-    public function middlewareUnless(bool|Closure $condition, MiddlewareInterface|array|string $middleware, int $priority = 0): static;
-    public function prependMiddleware(MiddlewareInterface|array|string $middleware, int $priority = 1000): static;
-    public function skipMiddleware(string $middlewareClass): static;
-    public function getMiddlewareStack(): iterable;
+    public function middleware(string|array $middleware): static;
+    public function getMiddleware(): array;
+    public function hasMiddleware(string $middlewareClass): bool;
+    public function clearMiddleware(): static;
     
     // Naming
     public function name(string $name): static;
