75c92a8d79bd32e169bf25682869678009e385bd
Deon Okonkwo <deondazy@gmail.com>
2025-09-28 04:17:40 +0100

    refactor: simplify middleware method signatures in RouterInterface

--- src/RouterInterface.php
+++ src/RouterInterface.php
@@ -6,7 +6,6 @@ namespace Denosys\Routing;
 
 use Closure;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 interface RouterInterface
@@ -19,11 +18,7 @@ interface RouterInterface
     public function options(string $pattern, Closure|array|string $handler): RouteInterface;
     public function any(string $pattern, Closure|array|string $handler): RouteInterface;
     public function match(string|array $methods, string $pattern, Closure|array|string $handler): RouteInterface;
-    public function middleware(MiddlewareInterface|array|string $middleware, int $priority = 0): static;
-    public function middlewareWhen(bool|Closure $condition, MiddlewareInterface|array|string $middleware, int $priority = 0): static;
-    public function middlewareUnless(bool|Closure $condition, MiddlewareInterface|array|string $middleware, int $priority = 0): static;
-    public function prependMiddleware(MiddlewareInterface|array|string $middleware, int $priority = 1000): static;
-    public function skipMiddleware(string $middlewareClass): static;
+    public function middleware(string|array $middleware): static;
     public function dispatch(ServerRequestInterface $request): ResponseInterface;
     public function group(string $prefix, Closure $callback): RouteGroupInterface;
     public function getRouteCollection(): RouteCollectionInterface;
